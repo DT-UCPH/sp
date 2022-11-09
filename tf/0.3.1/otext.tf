0d6ab2e1be5e51d18eb6eccc966d410a01dddcbb
ch-jensen <hojer88@gmail.com>
2022-11-09 19:44:18 +0100

    first nominal endings feature (nme)

---
+++ tf/0.3.1/otext.tf
@@ -0,0 +1,12 @@
+@config
+@compiler=Martijn Naaijer and Christian Canu Højgaard
+@fmt:text-orig-full={sign}
+@name=Samaritan Pentateuch - Genesis
+@purpose=First implementation of nominal endings (crude)
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@source=Stefan Schorch
+@version=0.3.1
+@writtenBy=Text-Fabric
+@dateWritten=2022-11-09T18:10:11Z
+
