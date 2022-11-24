815b2a7f3c2eb3fc7e5a670c49eb4dc19ac11763
ch-jensen <hojer88@gmail.com>
2022-11-24 17:31:42 +0100

    Verbal endings corrected

---
+++ tf/0.4.1/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=Corrections of verbal endings
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=0.4.1
+@writtenBy=Text-Fabric
+@dateWritten=2022-11-24T16:25:51Z
+
+1-100672	sign
+100673	book
+100674-100723	chapter
+100724-102259	verse
+102260-131199	word
