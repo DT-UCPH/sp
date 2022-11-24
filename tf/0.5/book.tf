baf3eaf9cad1ee1df8f83985780f9148161e8e39
ch-jensen <hojer88@gmail.com>
2022-11-24 20:12:42 +0100

    adding verbal preformatives (crude)

---
+++ tf/0.5/book.tf
@@ -0,0 +1,15 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@description=book title
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=Corrections of verbal endings
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=0.4.1
+@writtenBy=Text-Fabric
+@dateWritten=2022-11-24T19:07:43Z
+
+108807	Genesis
---
+++ tf/0.5/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=Corrections of verbal endings
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=0.4.1
+@writtenBy=Text-Fabric
+@dateWritten=2022-11-24T19:07:44Z
+
+1-108806	sign
+108807	book
+108808-108857	chapter
+108858-110393	verse
+110394-139333	word
