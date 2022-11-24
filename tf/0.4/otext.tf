20a317b8fa2614d3808011376a6d176095a502f7
ch-jensen <hojer88@gmail.com>
2022-11-24 16:17:55 +0100

    Verbal endings added (uncorrected)

---
+++ tf/0.4/otext.tf
@@ -0,0 +1,15 @@
+@config
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@fmt:text-orig-full={sign}
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=First addition of verbal endings (crude)
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@source=Stefan Schorch's data files, personal communication
+@version=0.4
+@writtenBy=Text-Fabric
+@dateWritten=2022-11-23T00:27:06Z
+
---
+++ tf/0.4/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=First addition of verbal endings (crude)
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=0.4
+@writtenBy=Text-Fabric
+@dateWritten=2022-11-23T00:27:06Z
+
+1-100300	sign
+100301	book
+100302-100351	chapter
+100352-101887	verse
+101888-130827	word
