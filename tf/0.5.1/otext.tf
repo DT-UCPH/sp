2193632c2f53a5343c2c15634d76299e61e6cd7e
ch-jensen <hojer88@gmail.com>
2022-11-25 14:36:36 +0100

    Verbal preformatives corrected

---
+++ tf/0.5.1/otext.tf
@@ -0,0 +1,15 @@
+@config
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@fmt:text-orig-full={sign}
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=Verbal preformatives corrected
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@source=Stefan Schorch's data files, personal communication
+@version=0.5.1
+@writtenBy=Text-Fabric
+@dateWritten=2022-11-25T13:33:25Z
+
