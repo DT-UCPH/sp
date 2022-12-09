82bf889e697e20379ca5cf67fc71756d1e2d9447
ch-jensen <hojer88@gmail.com>
2022-12-09 16:59:09 +0100

    Adding grammatical person to verbs and independent pronouns

---
+++ tf/0.9/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=Adding grammatical person to verbs and independent pronouns
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=0.9
+@writtenBy=Text-Fabric
+@dateWritten=2022-12-09T15:57:37Z
+
+1-100298	sign
+100299	book
+100300-100349	chapter
+100350-101885	verse
+101886-130820	word
