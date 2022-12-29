7c6079dc759f0b01f5df1ee95631514036a68f99
ch-jensen <hojer88@gmail.com>
2022-12-29 11:53:15 +0100

    Adding pronominal suffix person

---
+++ tf/1.0/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=Adding pronominal suffix person
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=1.0
+@writtenBy=Text-Fabric
+@dateWritten=2022-12-29T10:51:55Z
+
+1-100295	sign
+100296	book
+100297-100346	chapter
+100347-101882	verse
+101883-130814	word
