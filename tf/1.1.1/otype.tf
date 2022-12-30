dd7f055b92fdb0e25a2cc6f175c6b49f11876f74
ch-jensen <hojer88@gmail.com>
2022-12-30 13:27:23 +0100

    Correcting grammatical number

---
+++ tf/1.1.1/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=Correcting grammatical number
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=1.1.1
+@writtenBy=Text-Fabric
+@dateWritten=2022-12-30T12:26:38Z
+
+1-100319	sign
+100320	book
+100321-100370	chapter
+100371-101906	verse
+101907-130844	word
