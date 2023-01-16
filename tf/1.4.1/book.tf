cc380b394c8979018d806db605e0f1891b846c18
ch-jensen <hojer88@gmail.com>
2023-01-16 17:46:17 +0100

    Correcting pronominal suffix gender

---
+++ tf/1.4.1/book.tf
@@ -0,0 +1,15 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@description=book title
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=Adding pronominal suffix gender
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=1.4
+@writtenBy=Text-Fabric
+@dateWritten=2023-01-16T16:45:24Z
+
+100321	Genesis
