e15967d4468a5b0d854ac843a01cab70032c3069
ch-jensen <hojer88@gmail.com>
2023-01-17 15:11:14 +0100

    Correcting raw text and features

---
+++ tf/1.4.4/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=Correcting raw text plus features
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=1.4.3
+@writtenBy=Text-Fabric
+@dateWritten=2023-01-17T14:10:30Z
+
+1-100677	sign
+100678	book
+100679-100728	chapter
+100729-102264	verse
+102265-131315	word
