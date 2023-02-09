8aafe88fe90b375cff65fccba6f9cbd33e1ed596
ch-jensen <hojer88@gmail.com>
2023-02-09 18:04:31 +0100

    correcting morphemes g_lex, g_uvf

---
+++ tf/1.5.6/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=Correcting morphemes including g_lex and g_uvf
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=1.5.6
+@writtenBy=Text-Fabric
+@dateWritten=2023-02-09T17:00:54Z
+
+1-100676	sign
+100677	book
+100678-100727	chapter
+100728-102260	verse
+102261-131306	word
