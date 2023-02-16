33f0227fc691faf81ae1275a4641107ef75a8d54
Martijn Naaijer <martijn.naayer@chello.nl>
2023-02-16 13:25:12 +0100

    added features to test

--- tests/test_general.py
+++ tests/test_general.py
@@ -6,7 +6,7 @@ data_path = '/home/runner/work/sp/sp/tf'
 data_version = '1.5.5'
 TF = Fabric(locations=os.path.join(data_path, data_version))
 api = TF.load('''
-    otype lex sp
+    otype lex sp g_nme g_vbe g_pfm g_vbs g_prs vt ps nu prs_nu prs_ps
 ''')
 api.loadLog()
 api.makeAvailableIn(globals())
