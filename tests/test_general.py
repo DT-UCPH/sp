b087ffbec18ab184aff47c9bbfb5c64d981b25f8
Martijn Naaijer <martijn.naayer@chello.nl>
2023-02-16 12:31:56 +0100

    add tf stuff to test

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,6 +1,15 @@
 import os
 import pytest
 
+from tf.fabric import Fabric
+data_path = '~/tf/1.5.5'
+TF = Fabric(locations=data_path)
+api = TF.load('''
+    otype lex
+''')
+api.loadLog()
+api.makeAvailableIn(globals())
+
 
 def test_equality():
     print('Current WD', os.getcwd())
