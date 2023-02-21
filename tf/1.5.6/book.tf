0046d7b6937706002fca9ba348982be354c64c54
ch-jensen <hojer88@gmail.com>
2023-02-21 19:08:15 +0100

    correcting g_lex

---
+++ tf/1.5.6/book.tf
@@ -0,0 +1,15 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@description=book title
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=Correcting morphemes including g_lex and g_uvf
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=1.5.6
+@writtenBy=Text-Fabric
+@dateWritten=2023-02-21T18:06:59Z
+
+100677	Genesis
