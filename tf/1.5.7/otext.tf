88bbf3d33de38995bc061a0187044e88c192e311
ch-jensen <hojer88@gmail.com>
2023-02-22 19:21:46 +0100

    new tests

---
+++ tf/1.5.7/otext.tf
@@ -0,0 +1,15 @@
+@config
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@fmt:text-orig-full={sign}
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch vol 1. Genesis
+@purpose=Various corrections
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@source=Stefan Schorch's data files, personal communication
+@version=1.5.7
+@writtenBy=Text-Fabric
+@dateWritten=2023-02-22T18:14:45Z
+
