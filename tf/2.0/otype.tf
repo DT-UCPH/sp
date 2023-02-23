207e378511fb6cf254ed3d066b7ab4d0b1200fef
ch-jensen <hojer88@gmail.com>
2023-02-23 16:17:50 +0100

    Adding Exod-Deut and changing conversion script

---
+++ tf/2.0/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch
+@purpose=Adding Exodus to Deuteronomy
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=2.0
+@writtenBy=Text-Fabric
+@dateWritten=2023-02-23T15:15:11Z
+
+1-249831	sign
+249832-249836	book
+249837-250023	chapter
+250024-255864	verse
+255865-372541	word
