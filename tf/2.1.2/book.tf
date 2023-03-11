40c111e228541a44ff743f164644e03dbba5dad6
ch-jensen <hojer88@gmail.com>
2023-03-11 16:58:40 +0100

    update test and TF lex

---
+++ tf/2.1.2/book.tf
@@ -0,0 +1,19 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@description=book title
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch
+@purpose=Correction lex
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=2.1.2
+@writtenBy=Text-Fabric
+@dateWritten=2023-03-11T15:43:47Z
+
+401055	Genesis
+Exodus
+Leviticus
+Numbers
+Deuteronomy
---
+++ tf/2.1.2/otext.tf
@@ -0,0 +1,15 @@
+@config
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@fmt:text-orig-full={sign}
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch
+@purpose=Correction lex
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@source=Stefan Schorch's data files, personal communication
+@version=2.1.2
+@writtenBy=Text-Fabric
+@dateWritten=2023-03-11T15:44:01Z
+
