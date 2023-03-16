be01b20f40e893422fb1abe67b0c7e97955e7fc4
ch-jensen <hojer88@gmail.com>
2023-03-16 14:30:15 +0100

    adding g_nme (crude)

---
+++ tf/2.3.1/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch
+@purpose=g_nme added (crude)
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=2.3.1
+@writtenBy=Text-Fabric
+@dateWritten=2023-03-16T13:29:47Z
+
+1-399391	sign
+399392-399396	book
+399397-399583	chapter
+399584-405424	verse
+405425-520328	word
