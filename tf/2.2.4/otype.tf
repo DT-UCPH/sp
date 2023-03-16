ea166e4204251611fa12c67b8f90fb4323ae7c53
ch-jensen <hojer88@gmail.com>
2023-03-16 12:46:53 +0100

    Adding sp feature

---
+++ tf/2.2.4/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch
+@purpose=Adding part of speech (crude)
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=2.2.4
+@writtenBy=Text-Fabric
+@dateWritten=2023-03-16T10:54:42Z
+
+1-399391	sign
+399392-399396	book
+399397-399583	chapter
+399584-405424	verse
+405425-520328	word
