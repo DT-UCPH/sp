eb78a18118c20192ae85dc80fc76c91ab61a76d5
ch-jensen <hojer88@gmail.com>
2023-03-24 15:30:36 +0100

    Adding TF g_vbe feature

---
+++ tf/2.4.1/otext.tf
@@ -0,0 +1,15 @@
+@config
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@fmt:text-orig-full={sign}
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch
+@purpose=g_vbe added
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@source=Stefan Schorch's data files, personal communication
+@version=2.4.1
+@writtenBy=Text-Fabric
+@dateWritten=2023-03-22T09:07:04Z
+
