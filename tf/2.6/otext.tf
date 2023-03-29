c646dc7832c08fab29b0f87cf895df4fe622ac37
ch-jensen <hojer88@gmail.com>
2023-03-29 11:35:11 +0200

    adding TF 2.6

---
+++ tf/2.6/otext.tf
@@ -0,0 +1,15 @@
+@config
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@fmt:text-orig-full={sign}
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch
+@purpose=g_pfm added
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@source=Stefan Schorch's data files, personal communication
+@version=2.6
+@writtenBy=Text-Fabric
+@dateWritten=2023-03-29T09:28:37Z
+
