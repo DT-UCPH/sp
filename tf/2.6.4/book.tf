43e5f686a6cf312e4a3b912c1748fe16b274acf9
ch-jensen <hojer88@gmail.com>
2023-04-25 17:14:21 +0200

    new TF versions

---
+++ tf/2.6.4/book.tf
@@ -0,0 +1,19 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@description=book title
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch
+@purpose=g_pfm corrected
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=2.6.4
+@writtenBy=Text-Fabric
+@dateWritten=2023-04-19T08:42:32Z
+
+399392	Genesis
+Exodus
+Leviticus
+Numbers
+Deuteronomy
