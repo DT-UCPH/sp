fdb54cb90bd3a07351e439f6cdea3e879f973ac8
ch-jensen <hojer88@gmail.com>
2023-05-05 08:25:27 +0200

    correct g_lex

---
+++ tf/2.7.2/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Martijn Naaijer and Christian Canu Højgaard
+@createdBy=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@name=The Samaritan Pentateuch
+@purpose=g_lex corrected
+@source=Stefan Schorch's data files, personal communication
+@valueType=str
+@version=2.7.2
+@writtenBy=Text-Fabric
+@dateWritten=2023-05-05T06:20:50Z
+
+1-399392	sign
+399393-399397	book
+399398-399584	chapter
+399585-405425	verse
+405426-520338	word
