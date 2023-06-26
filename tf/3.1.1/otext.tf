47af3180c4ba7b734292616124a025f4450f0bdd
ch-jensen <hojer88@gmail.com>
2023-06-26 11:57:42 +0200

    adding ps feature

---
+++ tf/3.1.1/otext.tf
@@ -0,0 +1,16 @@
+@config
+@convertedToTextFabricBy=Martijn Naaijer and Christian Canu Højgaard
+@dataset=sp
+@datasetName=The Samaritan Pentateuch
+@encodedBy=Christian Canu Højgaard and Martijn Naaijer
+@fmt:text-orig-full={sign}
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=MS Dublin Chester Beatty Library 751 (Gen 1-Deut 32:36) + MS Garizim 1 (Deut 32:36b-34)
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@source=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@version=3.1.1
+@writtenBy=Text-Fabric
+@dateWritten=2023-06-22T14:48:26Z
+
---
+++ tf/3.1.2/otype.tf
@@ -0,0 +1,19 @@
+@node
+@convertedToTextFabricBy=Martijn Naaijer and Christian Canu Højgaard
+@dataset=sp
+@datasetName=The Samaritan Pentateuch
+@encodedBy=Christian Canu Højgaard and Martijn Naaijer
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=MS Dublin Chester Beatty Library 751 (Gen 1-Deut 32:36) + MS Garizim 1 (Deut 32:36b-34)
+@source=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@valueType=str
+@version=3.1.2
+@writtenBy=Text-Fabric
+@dateWritten=2023-06-26T09:23:00Z
+
+1-399391	sign
+399392-399396	book
+399397-399583	chapter
+399584-405424	verse
+405425-520321	word
