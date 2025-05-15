f30c6519b60532d45dc73ca6c9a64fe30897c605
Martijn N <martijn.naayer@chello.nl>
2025-05-15 11:31:52 +0200

    addition of phrase atoms

---
+++ tf/4.0/otype.tf
@@ -0,0 +1,22 @@
+@node
+@convertedToTextFabricBy=Martijn Naaijer, Christian Canu Højgaard and Saulo de Oliveira Cantanhêde
+@dataset=sp
+@datasetName=The Samaritan Pentateuch
+@dateWritten=2023-12-06T08:55:32Z
+@encodedBy=Christian Canu Højgaard and Martijn Naaijer
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=MS Dublin Chester Beatty Library 751 (Gen 1-Deut 32:36) + MS Garizim 1 (Deut 32:36b-34)
+@source=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@valueType=str
+@version=3.4.1
+@writtenBy=Text-Fabric
+@writtenBy=Text-Fabric
+@dateWritten=2025-05-14T20:25:50Z
+
+1-399392	sign
+399393-399397	book
+399398-399584	chapter
+399585-405425	verse
+405426-520316	word
+520317-590293	phrase_atom
