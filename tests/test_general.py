702cf80f60559d36e7ac0d415577fa79f52932d9
Saulo de Oliveira Cantanhêde <saulo.cantanhede@outlook.com>
2026-01-05 16:07:48 -0500

    Tests for clause atoms

--- tests/test_general.py
+++ tests/test_general.py
@@ -38,7 +38,7 @@ def reconstruct_lex(w):
     w = re.sub(r':?[a-z]', '', w) #Remove state and verbal stem
     return w
 
-# PHRASE-ATOM AND PHRASE LEVEL TESTS
+#PHRASE-ATOM AND PHRASE LEVEL TESTS
 def test_all_words_occur_in_one_phrase():
     assert all([len(L.u(w, 'phrase')) == 1 for w in F.otype.s('word')])
 
@@ -59,7 +59,20 @@ def test_last_word_of_phrase_is_last_word_of_phrase_atom():
     final_words_of_phrases = [L.d(ph, 'word')[-1] for ph in F.otype.s('phrase')]
     final_words_of_phrase_atoms = [L.d(pa, 'word')[-1] for pa in F.otype.s('phrase_atom')]
     all([node in final_words_of_phrase_atoms for node in final_words_of_phrases])
-    
+
+#CLAUSE-ATOM LEVEL TESTS
+def test_all_words_occur_in_one_clause_atom():
+    assert all([len(L.u(w, 'clause_atom')) == 1 for w in F.otype.s('word')])
+
+def test_last_word_of_verse_is_last_word_of_clause_atom():
+    final_words_of_verses = [L.d(ve, 'word')[-1] for ve in F.otype.s('verse')]
+    final_words_of_clause_atoms = [L.d(pa, 'word')[-1] for pa in F.otype.s('clause_atom')]
+    assert all([node in final_words_of_clause_atoms for node in final_words_of_verses])
+
+def test_last_word_of_clause_atom_is_last_word_of_phrase_atom():
+    final_words_of_clause_atoms = [L.d(ph, 'word')[-1] for ph in F.otype.s('clause_atom')]
+    final_words_of_phrase_atoms = [L.d(pa, 'word')[-1] for pa in F.otype.s('phrase_atom')]
+    all([node in final_words_of_phrase_atoms for node in final_words_of_clause_atoms])
 
 #WORD LEVEL TESTS
 def test_last_word_trailer():
