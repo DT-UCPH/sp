d9a929a96eca5131b6d339dfd06cf9ea8929a0f8
Saulo de Oliveira Cantanhêde <saulo.cantanhede@outlook.com>
2026-01-05 16:14:44 -0500

    update in clause atoms
    Updating verses with the same words, but different classification

---
+++ tf/6.0.1/otype.tf
@@ -0,0 +1,24 @@
+@node
+@convertedToTextFabricBy=Martijn Naaijer and Christian Canu Højgaard
+@dataset=sp
+@datasetName=The Samaritan Pentateuch
+@dateWritten=2025-12-03T19:54:12+00:00Z
+@encodedBy=Christian Canu Højgaard, Saulo de Oliveira Cantanhêde, and Martijn Naaijer
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=MS Dublin Chester Beatty Library 751 (Gen 1-Deut 32:36) + MS Garizim 1 (Deut 32:36b-34)
+@source=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@valueType=str
+@version=5.0.4
+@writtenBy=Text-Fabric
+@writtenBy=Text-Fabric
+@dateWritten=2026-01-05T19:36:33Z
+
+1-399392	sign
+399393-399397	book
+399398-399584	chapter
+399585-405425	verse
+405426-520314	word
+520315-590298	phrase_atom
+590299-656705	phrase
+656706-678971	clause_atom
