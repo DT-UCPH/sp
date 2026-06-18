98322c0c29759c84232ce5614d3140017c41963f
Saulo de Oliveira Cantanhêde <saulo.cantanhede@outlook.com>
2026-06-18 13:00:57 -0400

    updating phrase type features
    Updating phrase type feature of verses with same length, but different classification.
    
    Clause edited:
    Exodus 34:10; Leviticus 13:45
    
    Clause atom edited: Exodus 34:10; Leviticus 13:45
    
    Phrase edited: Genesis 21:22; Genesis 42:38; Genesis 13:18; Genesis 34:31; Numbers 24:22; Exodus 34:10; Leviticus 13:45; Genesis 16:8; Genesis 19:9 - גם + verb in infinitive absolute should be separated; Exodus 26:8; Exodus 15:22
    
    Phrase atom edited: Genesis 34:31; Numbers 24:22; Exodus 34:10; Leviticus 13:45; Genesis 19:9 - גם + verb in infinitive absolute should be separated; Exodus 26:8; Exodus 15:22

---
+++ tf/7.1.2/otype.tf
@@ -0,0 +1,25 @@
+@node
+@convertedToTextFabricBy=Martijn Naaijer and Christian Canu Højgaard
+@dataset=sp
+@datasetName=The Samaritan Pentateuch
+@dateWritten=2026-06-18T15:47:53Z
+@encodedBy=Christian Canu Højgaard, Saulo de Oliveira Cantanhêde, and Martijn Naaijer
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=MS Dublin Chester Beatty Library 751 (Gen 1-Deut 32:36) + MS Garizim 1 (Deut 32:36b-34)
+@source=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@valueType=str
+@version=7.1.2
+@writtenBy=Text-Fabric
+@writtenBy=Text-Fabric
+@dateWritten=2026-06-18T15:49:05Z
+
+1-399392	sign
+399393-399397	book
+399398-399584	chapter
+399585-405425	verse
+405426-520314	word
+520315-542060	clause
+542061-564608	clause_atom
+564609-630673	phrase
+630674-700584	phrase_atom
