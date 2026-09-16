ec8c453021a4001f005c8b84b3fd529e4f734be3
Saulo de Oliveira Cantanhêde <saulo.cantanhede@outlook.com>
2026-09-15 21:51:43 -0300

    update verses different length
    Updating phrase function features in SP for verses with different length
    
    Clause updated:
    Numbers 21:30, Genesis 38:21, Numbers 22:35, Deuteronomy 13:19, Genesis 31:12, Leviticus 11:3, Leviticus 6:13, Genesis 6:20, Numbers 24:23, Leviticus 15:3, Exodus 23:19, Leviticus 17:4, Exodus 18:25, Genesis 30:36
    
    Clause atom updated:
    Numbers 21:30, Genesis 38:21, Numbers 22:35, Deuteronomy 13:19, Genesis 31:12, Leviticus 11:3, Leviticus 6:13, Leviticus 3:5, Genesis 6:20, Numbers 24:23, Leviticus 15:3, Exodus 23:19, Leviticus 17:4, Exodus 18:25, Genesis 30:36, Leviticus 11:3, Numbers 10:36
    
    Phrase type updated:
    Deuteronomy 3:21, Deuteronomy 14:17, Deuteronomy 28:14, Deuteronomy 11:14, Numbers 4:19, Exodus 27:1, Leviticus 21:10, Genesis 22:13, Genesis 24:21, Genesis 44:11, Numbers 18:17, Genesis 29:9, Genesis 31:42, Genesis 44:20, Exodus 22:12, Exodus 22:30, Exodus 31:11, Exodus 39:29, Leviticus 11:42, Numbers 21:29, Genesis 1:26, Genesis 43:14, Leviticus 11:38, Numbers 7:85, Numbers 21:30, Genesis 4:17, Genesis 11:26, Genesis 26:18, Genesis 40:21, Exodus 5:5, Numbers 23:10, Numbers 23:25, Numbers 24:10, Deuteronomy 4:42, Deuteronomy 21:3, Genesis 17:24, Exodus 21:36, Leviticus 5:8, Exodus 23:11, Numbers 22:35, Numbers 30:6, Genesis 15:10, Genesis 15:10, Leviticus 6:8, Deuteronomy 13:19, Deuteronomy 32:9, Genesis 31:12, Exodus 28:12, Leviticus 6:13, Leviticus 26:39, Numbers 5:20, Genesis 48:7, Genesis 6:20, Numbers 24:23, Numbers 28:27, Exodus 21:35, Genesis 50:5, Deuteronomy 14:8, Exodus 29:5, Leviticus 15:3, Exodus 23:19, Exodus 32:10, Numbers 21:12, Deuteronomy 10:7, Exodus 18:25, Numbers 31:20, Exodus 20:19, Deuteronomy 5:21, Exodus 26:35, Numbers 20:13
    
    Phrase updated:
    Numbers 4:19, Exodus 27:1, Exodus 22:12, Exodus 31:11, Leviticus 11:42, Genesis 1:26, Numbers 7:85, Numbers 23:25, Deuteronomy 4:42, Exodus 21:36, Exodus 23:11, Genesis 15:10, Deuteronomy 13:19, Deuteronomy 32:9, Leviticus 6:13, Leviticus 26:39, Genesis 48:7, Numbers 28:27, Leviticus 15:3, Exodus 18:25
    
    Phrase atom updated:
    Numbers 4:19, Leviticus 11:42, Numbers 23:25, Deuteronomy 4:42, Exodus 21:36, Exodus 23:11, Deuteronomy 13:19, Leviticus 6:13, Leviticus 15:3

---
+++ tf/7.2.4/otype.tf
@@ -0,0 +1,25 @@
+@node
+@convertedToTextFabricBy=Martijn Naaijer and Christian Canu Højgaard
+@dataset=sp
+@datasetName=The Samaritan Pentateuch
+@dateWritten=2026-09-16T00:31:33Z
+@encodedBy=Christian Canu Højgaard, Saulo de Oliveira Cantanhêde, and Martijn Naaijer
+@licence=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenceUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@manuscripts=MS Dublin Chester Beatty Library 751 (Gen 1-Deut 32:36) + MS Garizim 1 (Deut 32:36b-34)
+@source=Stefan Schorch in colloboration with Evelyn Burkhardt, Ulrike Hirschfelder, Irina Wandrey and József Zsengellér
+@valueType=str
+@version=7.2.4
+@writtenBy=Text-Fabric
+@writtenBy=Text-Fabric
+@dateWritten=2026-09-16T00:32:44Z
+
+1-399392	sign
+399393-399397	book
+399398-399584	chapter
+399585-405425	verse
+405426-520314	word
+520315-542050	clause
+542051-564580	clause_atom
+564581-630621	phrase
+630622-700524	phrase_atom
